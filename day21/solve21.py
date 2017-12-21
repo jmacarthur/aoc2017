4e69bdf543c8e9ecdd93dd83515d5ab20dba0e92
Jim MacArthur <jim+git@mode7.co.uk>
2017-12-21 23:58:53 +0000

    Add input pattern cacheing

--- day21/solve21.py
+++ day21/solve21.py
@@ -5,12 +5,29 @@ import sys
 import logging
 logging.basicConfig(level=logging.INFO)
 
+transform_cache_2 = {}
+transform_cache_3 = {}
+
 class Sprite(object):
     def __init__(self, size):
         self.size = size
         self.pixels = []
         for y in range(0,size):
             self.pixels.append([0]*size)
+    def integer_val(self,ox,oy,size):
+        bit = 0
+        val = 0
+        for x in range(0,size):
+            for y in range(0,size):
+               val |= (self.pixels[oy+y][ox+x] << bit)
+               bit += 1
+        return val
+    @staticmethod
+    def pixelarray(size):
+        pixels = []
+        for y in range(0,size):
+            pixels.append([0]*size)
+        return pixels
     @staticmethod
     def convert_line(line):
         return [1 if x=='#' else 0 for x in line]
@@ -26,10 +43,9 @@ class Sprite(object):
         n.pixels = copy.deepcopy(self.pixels)
         return n
     def direct_match_flip_v(self, pattern, x, y, size):
-        for px in range(0,size):
-            for py in range(0,size):
-                if self.pixels[y+py][x+px] != pattern.pixels[size-1-py][px]:
-                    return False
+        for py in range(0,size):
+            if self.pixels[y+py][x:x+size] != pattern.pixels[size-1-py]:
+                return False
         logging.debug("Match!")
         return True
     def direct_match_flip_h(self, pattern, x, y, size):
@@ -40,20 +56,19 @@ class Sprite(object):
         logging.debug("Match!")
         return True
     def direct_match(self, pattern, x, y, size):
-        for px in range(0,size):
-            for py in range(0,size):
-                if self.pixels[y+py][x+px] != pattern.pixels[py][px]:
-                    return False
+        for py in range(0,size):
+            if self.pixels[y+py][x:x+size] != pattern.pixels[py]:
+                return False
         logging.debug("Match!")
         return True
     def rotate_ccw(self):
-        n = self.duplicate()
+        n = Sprite.pixelarray(self.size)
         for row in range(0,self.size):
             for col in range(0,self.size):
-                n.pixels[self.size-1-col][row] = self.pixels[row][col]
-        self.pixels = n.pixels
+                n[self.size-1-col][row] = self.pixels[row][col]
+        self.pixels = n
     def match(self, x, y, size, pattern):
-        logging.debug("Looking for %r at %d,%d"%(pattern,x,y))
+        logging.debug("Looking for %r at %d,%d",pattern,x,y)
         if pattern.size != size: return False
         p = pattern.duplicate()
         for d in range(0,4):
@@ -88,12 +103,24 @@ def apply_transforms(sprite, x, y, size, transforms,destination):
     """Try to apply each of the transforms in order until one fits. x and
     y are cell coordinates and must be multiplied by the cell size to get
     pixels."""
+    origin_int = sprite.integer_val(x*size, y*size, size)
+    if size==2 and origin_int in transform_cache_2:
+        destination.blit(transform_cache_2[origin_int], x*(size+1), y*(size+1))
+        return True
+    elif size==3 and origin_int in transform_cache_3:
+        destination.blit(transform_cache_3[origin_int], x*(size+1), y*(size+1))
+        return True
+
     for (start,finish) in transforms:
         if sprite.match(x*size,y*size,size,start):
-            logging.debug("Original: %r"%destination)
-            logging.debug("Blitting %r at %d, %d"%(finish,x,y))
+            if size == 2:
+                transform_cache_2[origin_int] = finish
+            elif size == 3:
+                transform_cache_3[origin_int] = finish
+            logging.debug("Original: %r",destination)
+            logging.debug("Blitting %r at %d, %d",finish,x,y)
             destination.blit(finish, x*(size+1), y*(size+1))
-            logging.debug("Result: %r"%destination)
+            logging.debug("Result: %r",destination)
             return True
     return False
 
@@ -104,7 +131,7 @@ def iterate(sprite, transforms_2, transforms_3):
         for x in range(0,cells):
             for y in range(0,cells):
                 if not apply_transforms(sprite, x, y, 2, transforms_2, destination):
-                    logging.error("No pattern matched at cell %d, %d"%(x,y))
+                    logging.error("No pattern matched at cell %d, %d",x,y)
                     sys.exit(1)
     elif sprite.size % 3 == 0:
         cells = sprite.size / 3
@@ -112,7 +139,7 @@ def iterate(sprite, transforms_2, transforms_3):
         for x in range(0,cells):
             for y in range(0,cells):
                 if not apply_transforms(sprite, x, y, 3, transforms_3, destination):
-                    logging.error("No pattern matched at cell %d, %d"%(x,y))
+                    logging.error("No pattern matched at cell %d, %d",x,y)
                     sys.exit(1)
     else:
         logging.error("Illegal sprite size %d"%sprite.size)
@@ -136,7 +163,7 @@ def main():
     s = Sprite(3)
     s.pixels = [[0,1,0], [0,0,1], [1,1,1]]
     for i in range(0,18):
-        logging.info("----- Iteration %d starts with:\n"%i)
+        logging.info("----- Iteration %d; size %d"%(i, s.size))
         s = iterate(s, transforms_2, transforms_3)
         logging.info("New pattern has %d pixels"%s.count_pixels())
 
